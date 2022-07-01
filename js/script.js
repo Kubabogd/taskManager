9aac68e02fb4d273238b64fa59392837a580e682
Jakub Bogdanski <bogdanskikuba@gmail.com>
2022-07-01 13:53:06 +0200

    immutability added

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
 {
-    const tasks = [
+    let tasks = [
     ];
     const clearField = () => {
         document.querySelector(".js-newTask").value = "";
         document.querySelector(".js-newTask").focus();
-    }
-    
+    };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
         clearField();
         render();
     };
-
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(taskIndex, taskIndex + 1)
+        ];
         clearField();
         render();
-    }
-
+    };
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(taskIndex),
+            { ...tasks[taskIndex] },
+            ...tasks.slice(editIndex + 1)
+        ];
         render();
-    }
-
+    };
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
-
         removeButtons.forEach((removeButtons, index) => {
             removeButtons.addEventListener("click", () => {
                 removeTask(index);
             });
         });
-
         const toggleDoneButtons = document.querySelectorAll(".js-done");
 
         toggleDoneButtons.forEach((toggleDoneButtons, index) => {
@@ -42,8 +43,7 @@
                 toggleTaskDone(index);
             });
         });
-    }
-
+    };
     const render = () => {
         let htmlString = "";
 
@@ -51,48 +51,27 @@
             htmlString += ` 
             <li class="sectionTaskList__newTask"; 
             >
-            
         <button class="section__buttonDone js-done">${task.done ? "✓" : ""}</button>
         <span class="js-taskDone${task.done ? " section__buttonDone--checked " : ""}">${task.content}</span>
         <button class="section__buttonDelete js-remove">🗑</button>
         </li>
         `;
         }
-
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
         bindEvents();
     };
-
     const onFormSubmit = (event) => {
         event.preventDefault();
-
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
         if (newTaskContent === "") {
             return;
         }
         addNewTask(newTaskContent);
     };
-
     const init = () => {
         render();
-
         const form = document.querySelector(".js-form");
-
         form.addEventListener("submit", onFormSubmit);
     };
-
     init();
 }
-
-
-
-
-
-
-
-
-
-
-
